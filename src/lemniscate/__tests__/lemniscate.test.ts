47b07cd064a48ed9fba0fa4acce5f4ba6fd218a4
era <era.sundry@gmail.com>
2024-09-30 18:59:04 -0700

    all tests

--- src/lemniscate/__tests__/lemniscate.test.ts
+++ src/lemniscate/__tests__/lemniscate.test.ts
@@ -1,11 +1,62 @@
+import { initDocument } from "../../test_helpers";
+import { Lemniscate, LemniscateOps } from "../lemniscate";
+
+const properties = [
+  "arcs",
+  "arcWidth",
+  "arcWidthDelta",
+  "tailDelay",
+  "arcDelay",
+  "radius",
+  "radiusDelta",
+  "xOff",
+];
+
+const modifiedOps: Partial<LemniscateOps> = {
+  arcs: 2,
+  arcWidth: 20,
+  arcWidthDelta: 0.2,
+  tailDelay: 0.2,
+  arcDelay: 0.2,
+  radius: 20,
+  radiusDelta: 0.2,
+  xOff: 30,
+};
+
+test("empty constructor populates all option fields", () => {
+  initDocument();
+  const instance = new Lemniscate("canvas");
+  expect(instance).toBeInstanceOf(Lemniscate);
+  for (const prop of properties) {
+    expect(instance).toHaveProperty(prop);
+  }
+  expect(instance.canvas.width).toEqual(100);
+  expect(instance.canvas.height).toEqual(100);
+  expect(instance.postConstructorCalls).toEqual(1);
+  expect(instance.paused).toBe(false);
+});
+
 test("all options work properly in constructor", () => {
-  //TODO:
+  initDocument();
+  for (const objKey of Object.keys(modifiedOps)) {
+    const ops = structuredClone(modifiedOps) as any;
+    const partial = {} as any;
+    partial[objKey] = ops[objKey];
+    const instance = new Lemniscate("canvas", partial) as any;
+    expect(instance).toHaveProperty(objKey);
+    expect(instance[objKey]).toEqual(ops[objKey]);
+  }
 });
 
 test("all options work properly in setConfig", () => {
-  //TODO:
-});
-
-test("correct number of context draws per draw call", () => {
-  //TODO:
+  initDocument();
+  for (const objKey of Object.keys(modifiedOps)) {
+    const ops = structuredClone(modifiedOps) as any;
+    const instance = new Lemniscate("canvas") as any;
+    const partial = {} as any;
+    partial[objKey] = ops[objKey];
+    instance.setConfig(partial);
+    expect(instance).toHaveProperty(objKey);
+    expect(instance[objKey]).toEqual(ops[objKey]);
+  }
 });
--- src/sempiternal/__tests__/sempiternal.test.ts
+++ src/sempiternal/__tests__/sempiternal.test.ts
@@ -1,11 +1,70 @@
+import { initDocument } from "../../test_helpers";
+import { Sempiternal, SempiternalOps } from "../sempiternal";
+
+const properties = [
+  "sideLength",
+  "circleSize",
+  "relativeExpansion",
+  "delay",
+  "alternateSpin",
+  "rotations",
+  "opacityPulse",
+  "radiusPulse",
+];
+
+const modifiedOps: Partial<SempiternalOps> = {
+  sideLength: 4,
+  circleSize: 19,
+  relativeExpansion: 0.12,
+  delay: 0.12,
+  alternateSpin: true,
+  rotations: 2,
+  opacityPulse: {
+    style: "disperse",
+    delay: 0.1,
+    intensity: 1,
+  },
+  radiusPulse: {
+    style: "disperse",
+    delay: 0.1,
+    intensity: 1,
+  },
+};
+
+test("empty constructor populates all option fields", () => {
+  initDocument();
+  const instance = new Sempiternal("canvas");
+  expect(instance).toBeInstanceOf(Sempiternal);
+  for (const prop of properties) {
+    expect(instance).toHaveProperty(prop);
+  }
+  expect(instance.canvas.width).toEqual(100);
+  expect(instance.canvas.height).toEqual(100);
+  expect(instance.postConstructorCalls).toEqual(1);
+  expect(instance.paused).toBe(false);
+});
+
 test("all options work properly in constructor", () => {
-  //TODO:
+  initDocument();
+  for (const objKey of Object.keys(modifiedOps)) {
+    const ops = structuredClone(modifiedOps) as any;
+    const partial = {} as any;
+    partial[objKey] = ops[objKey];
+    const instance = new Sempiternal("canvas", partial) as any;
+    expect(instance).toHaveProperty(objKey);
+    expect(instance[objKey]).toEqual(ops[objKey]);
+  }
 });
 
 test("all options work properly in setConfig", () => {
-  //TODO:
-});
-
-test("correct number of context draws per draw call", () => {
-  //TODO:
+  initDocument();
+  for (const objKey of Object.keys(modifiedOps)) {
+    const ops = structuredClone(modifiedOps) as any;
+    const instance = new Sempiternal("canvas") as any;
+    const partial = {} as any;
+    partial[objKey] = ops[objKey];
+    instance.setConfig(partial);
+    expect(instance).toHaveProperty(objKey);
+    expect(instance[objKey]).toEqual(ops[objKey]);
+  }
 });
